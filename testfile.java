c5bfe369e281bbbf3826bc23977d59193e035a53
Aravind <Aravind@gmail.com>
2026-06-04 01:38:04 +0000

    testfile

---
+++ testfile.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
